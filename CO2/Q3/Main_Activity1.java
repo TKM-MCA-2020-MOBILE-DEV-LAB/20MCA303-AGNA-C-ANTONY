19509097ee12dba1cf8704bf9692e04ca7bc08ab
AGNA2812 <74620131+AGNA2812@users.noreply.github.com>
2022-01-20 22:10:34 +0530

    q3

---
+++ CO2/Q3/Main_Activity1.java
@@ -0,0 +1,13 @@
+MainActivity.java:
+package com.example.relativelayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
