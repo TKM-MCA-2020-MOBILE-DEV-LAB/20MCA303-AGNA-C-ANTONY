fb39a591939c5d5870205165e4624795d43096ff
AGNA2812 <74620131+AGNA2812@users.noreply.github.com>
2022-01-22 04:27:37 +0530

    q4

---
+++ CO3/Q4/optionsall.java
@@ -0,0 +1,64 @@
+Home.java
+
+package com.example.optionmenu;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
+
+
+about.java
+
+package com.example.optionmenu;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
+
+settings.java
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
+
+contact.java
+
+package com.example.optionmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+}
